27c8e4534713eb6556c535bbf3472231977f7c14
Crina Golcea <crina@digitalgarden.ro>
2022-12-09 08:31:04 +0200

    gradient background

---
+++ src/components/HomeSection/HomeSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { 
+    Container,
+} from "./HomeSection.style";
+
+
+const HomeSection = () => {
+    return (
+        <Container>
+        </Container>
+    )   
+}
+
+export default HomeSection;
---
+++ src/components/HomeSection/HomeSection.style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    margin: auto;
+    width: 1320px;
+    height: 500px;
+    background: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(200, 255, 228, 0.7) 100%);
+    justify-content: center;
+`;
+
