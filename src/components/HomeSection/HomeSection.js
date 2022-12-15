ebdeaa7565f9385cc916622deb369d101c468f29
Crina Golcea <crina@digitalgarden.ro>
2022-12-15 09:53:41 +0200

    finish home title

--- src/components/HomeSection/HomeSection.js
+++ src/components/HomeSection/HomeSection.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { 
     Container,
+    HomeTitle,
+    LeafAndLine
 } from "./HomeSection.style";
+import leaf from "../../svg/S1 Leaf.svg";
+import line from "../../svg/Line 3.svg";
 
 
 const HomeSection = () => {
     return (
         <Container>
+            <LeafAndLine>
+            <img class = "leaf" src={leaf} alt = "leaf"/>
+            <img class = "line" src={line} alt = "line"/>
+            </LeafAndLine>
+            <HomeTitle>
+                <p class = "firstTitle">WELCOME <span>IN THE</span></p>
+                <p class = "thirdTitle">DIGITAL GARDEN</p>
+            </HomeTitle>
         </Container>
     )   
 }
--- src/components/HomeSection/HomeSection.style.js
+++ src/components/HomeSection/HomeSection.style.js
@@ -1,10 +1,55 @@
 import styled from "styled-components";
 
+
 export const Container = styled.div`
     margin: auto;
     width: 1320px;
     height: 500px;
     background: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(200, 255, 228, 0.7) 100%);
+    display: flex;
     justify-content: center;
+    margin-top: 8%;
 `;
+
+export const HomeTitle = styled.div`
+    text-align: center;
+    font-size: 54px;
+    line-height: 73.66px;
+  
+    p {
+        margin: 0;
+    }
+    .firstTitle {
+        font-weight: 500;
+        color:#3C3C3C;
+        margin-top: 7px;
+    }
+
+    span {
+        font-weight: 400;
+        color :#4D4D4D;
+    }
+
+    .thirdTitle {
+        font-weight: 800;
+        color: #2EA15C;
+    }
+`;
+
+
+export const LeafAndLine = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    .leaf {
+        margin-right: 23px;
+    }
+
+    .line {
+        height: 113px;
+    }
+`;
+
+
+
 
