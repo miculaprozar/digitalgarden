a51013fb58e79e77dac43567b1ba37cfbd7b7c54
GolceaCrinaTeodora <118722654+GolceaCrinaTeodora@users.noreply.github.com>
2022-12-20 11:48:54 +0200

    finished the home section (#7)

--- src/components/HomeSection/HomeSection.js
+++ src/components/HomeSection/HomeSection.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { Container } from "../../styles/Global";
 import { 
-    Container,
+    ContainerHome,
     HomeTitle,
     LeafAndLine,
     Description,
@@ -8,29 +9,33 @@ import {
 } from "./HomeSection.style";
 import leaf from "../../svg/S1 Leaf.svg";
 import line from "../../svg/Line 3.svg";
-import illustration from "../../svg/Hero Section Illustration.svg"
-import arrow from "../../svg/Vector.svg"
+import illustration from "../../svg/Hero Section Illustration.svg";
+import arrow from "../../svg/Vector.svg";
+import bgShape from "../../svg/S1 South BG Shape.svg"
 
 const HomeSection = () => {
     return (
         <Container>
-            <LeafAndLine>
-            <img class = "leaf" src={leaf} alt = "leaf"/>
-            <img class = "line" src={line} alt = "line"/>
-            </LeafAndLine>
-            <HomeTitle>
-                <p class = "firstTitle">WELCOME <span>IN THE</span></p>
-                <p class = "thirdTitle">DIGITAL GARDEN</p>
-                <Description>
-                    Right here, our mission is to help you cultivate performance and
-                    <br></br>asure the growth of your desired digital product. 
-                </Description>
-                <Button>
-                    Our Work
-                    <img class = "arrow" src={arrow} alt = "arrow"/>
-                </Button>
-            </HomeTitle>
-            <img class = "illustration" src={illustration} alt="illustration"/>
+            <ContainerHome>
+                <LeafAndLine>
+                <img class = "leaf" src={leaf} alt = "leaf"/>
+                <img class = "line" src={line} alt = "line"/>
+                </LeafAndLine>
+                <HomeTitle>
+                    <p class = "firstTitle">WELCOME <span>IN THE</span></p>
+                    <p class = "thirdTitle">DIGITAL GARDEN</p>
+                    <Description>
+                        Right here, our mission is to help you cultivate performance and
+                        <br></br>asure the growth of your desired digital product. 
+                    </Description>
+                    <Button>
+                        Our Work
+                        <img class = "arrow" src={arrow} alt = "arrow"/>
+                    </Button>
+                </HomeTitle>
+                <img class = "illustration" src={illustration} alt="illustration"/>
+            </ContainerHome>
+            <img alt="bgShape" src={bgShape}/>
         </Container>
 
     )   
