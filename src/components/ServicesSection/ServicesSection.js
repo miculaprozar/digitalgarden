be37ff9977d9d69a4f02fc8601fe8d0196391428
Crina Golcea <crina@digitalgarden.ro>
2022-12-21 10:24:20 +0200

    added the text under the services button

--- src/components/ServicesSection/ServicesSection.js
+++ src/components/ServicesSection/ServicesSection.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Button } from "./ServicesSection.style";
 import { Container } from "../../styles/Global";
 import { ContainerServices } from "./ServicesSection.style";
+import { Text } from "./ServicesSection.style";
 
 const Services = () => {
 	return (
 		<Container>
 			<ContainerServices>
 				<Button>SERVICES</Button>
+				<Text>
+					WE OFFER TO YOU THE POSIBILITY OF DEVELOPING WEB AND MOBILE APPS
+				</Text>
 			</ContainerServices>
 		</Container>
 	);
--- src/components/ServicesSection/ServicesSection.style.js
+++ src/components/ServicesSection/ServicesSection.style.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
+	margin-top: 80px;
 	width: 200px;
 	height: 49px;
 	background: rgba(217, 255, 237, 0.41);
@@ -13,7 +14,16 @@ export const Button = styled.button`
 	color: #4d4d4d;
 `;
 
+export const Text = styled.p`
+	margin-top: 48px;
+	font-weight: 700;
+	font-size: 16px;
+	line-height: 22px;
+	color: #7b7b7b;
+`;
+
 export const ContainerServices = styled.div`
-	margin: auto;
-	display: table;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 `;
