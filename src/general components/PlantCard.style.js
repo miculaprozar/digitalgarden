d8c6a0dfab9bbef1526dff707ca24eb182a508a9
Crina Golcea <crina@digitalgarden.ro>
2023-01-05 12:44:01 +0200

    finished plant card

---
+++ src/general components/PlantCard.style.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+	position: relative;
+	width: 198px;
+	height: 164px;
+	background: #ffffff;
+	box-shadow: 0px 0px 10px rgba(4, 41, 5, 0.23);
+	border-radius: 24px;
+	.circle {
+		padding: 22px 39px;
+	}
+	.content {
+		position: absolute;
+		top: 37%;
+		left: 37%;
+	}
+`;
