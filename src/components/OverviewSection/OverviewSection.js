3d9cce56c11339c3e2cc490344dc60ff17247c0e
Crina Golcea <crina@digitalgarden.ro>
2023-01-17 15:20:59 +0200

    finished subtitle and image part

--- src/components/OverviewSection/OverviewSection.js
+++ src/components/OverviewSection/OverviewSection.js
@@ -1,7 +1,19 @@
 import React from "react";
 import { Container } from "../../styles/Global";
-import { Overview, Line, Text, Tag } from "./OverviewSection.style";
+import {
+	Overview,
+	Line,
+	Text,
+	Tag,
+	Description,
+	Subtitles,
+	SubtitlesGroup,
+	Screen,
+	InsideScreen,
+	Speaker,
+} from "./OverviewSection.style";
 import rectangle from "../../svg/Rectangle 82.svg";
+import screen from "../../images/MA screen image.png";
 
 const OverviewSection = () => {
 	return (
@@ -13,6 +25,28 @@ const OverviewSection = () => {
 					<Text>OVERVIEW</Text>
 				</Tag>
 				<Line />
+				<Description>
+					<Subtitles>
+						<SubtitlesGroup>
+							<p>Place</p>
+							<h3>Romania</h3>
+						</SubtitlesGroup>
+						<SubtitlesGroup>
+							<p>Category</p>
+							<h3>E-commerce</h3>
+						</SubtitlesGroup>
+						<SubtitlesGroup>
+							<p>System</p>
+							<h3>Android, Web</h3>
+						</SubtitlesGroup>
+					</Subtitles>
+					<Screen>
+						<Speaker />
+						<InsideScreen>
+							<img src={screen} alt="screen" />
+						</InsideScreen>
+					</Screen>
+				</Description>
 			</Overview>
 		</Container>
 	);
--- src/components/OverviewSection/OverviewSection.style.js
+++ src/components/OverviewSection/OverviewSection.style.js
@@ -28,4 +28,52 @@ export const Text = styled.div`
 export const Tag = styled.div`
 	display: flex;
 	margin: 40px 0;
+	gap: 30px;
+`;
+
+export const Description = styled.div`
+	img {
+		box-shadow: inset 0px 4px 12px rgba(0, 0, 0, 0.25);
+		border-radius: 12px;
+		background: rgba(255, 255, 255, 0.0001);
+		border-radius: 22px;
+	}
+	margin: 51px 0;
+`;
+
+export const Subtitles = styled.div`
+	display: flex;
+	gap: 56px;
+`;
+
+export const SubtitlesGroup = styled.div`
+	h3,
+	p {
+		margin: 0;
+	}
+`;
+
+export const InsideScreen = styled.div`
+	width: 234.37px;
+	height: 348.43px;
+	background: rgba(255, 255, 255, 0.0001);
+	border-radius: 22px;
+`;
+export const Screen = styled.div`
+	width: 262.01px;
+	height: 367.01px;
+	background: linear-gradient(180deg, #ffffff 32.38%, #eef4fa 100%);
+	box-shadow: inset -6px -4px 16px #c7c7c7, inset 15px 0px 12px 3px #f4f4f4,
+		inset 0px -4px 5px #6cf48a, inset -7px -10px 34px #daffde;
+	border-radius: 41px;
+	float: right;
+	padding: 18px 20px;
+`;
+
+export const Speaker = styled.div`
+	width: 67.91px;
+	height: 2.48px;
+	background: #e5f9e4;
+	border-radius: 9px;
+	margin: 0 69px 17px;
 `;
